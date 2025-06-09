5e16bd7d960804c99b72820ecf02232ac4254cfb
RVLK <edumwID_19@hotmail.com>
2025-06-09 12:59:58 -0600

    Corrección de direcciones

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ function validateForm() {
     // Aquí podrías agregar validación adicional si es necesario (ejemplo: verificar si el correo tiene formato correcto)
 
     // Redirigir al index.html
-    window.location.href = "/pages/index.html";  // Cambia esto a la URL de tu página principal
+    window.location.href = "index.html";  // Cambia esto a la URL de tu página principal
 
     return false;  // Prevenir que el formulario se envíe y recargue la página
 }
